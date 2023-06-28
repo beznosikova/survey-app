00995f30860db56acf678be37e2b2c680d29df9c
Tetiana Beznosikova <beznosikovat@gmail.com>
2023-06-28 13:02:06 +0200

    Update views for surveys

--- resources/views/surveys/forms/edit.blade.php
+++ resources/views/surveys/forms/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="divide-y divide-gray-200 dark:divide-gray-700">
         <div class="space-y-2 pt-6 pb-8 md:space-y-5">
             <h1
-                class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                class="text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 ">
                 Edycja badania
             </h1>
 
--- resources/views/surveys/index.blade.php
+++ resources/views/surveys/index.blade.php
@@ -6,7 +6,7 @@
 
         <div class="space-y-2 pt-6 pb-8 md:space-y-5">
             <h1
-                class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                class="text-3xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 ">
                 Lista badań
             </h1>
             <p class="text-lg leading-7 text-gray-500 dark:text-gray-400">
